51f1bfbab5b6909c33eacfc7855875409c09eefb
Charles Dellinger <charles.dellinger@gmail.com>
2022-02-23 22:22:20 -0500

    added domain extension and payment for registering domains to Domains.sol and tests in run.js

--- cool-domains/contracts/Domains.sol
+++ cool-domains/contracts/Domains.sol
@@ -2,9 +2,32 @@
 
 pragma solidity ^0.8.10;
 
+// Don't forget to add this import
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+    // Here's our domain TLD!
+    string public tld;
+
+    // We make the contract "payable" by adding this to the constructor
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("%s name service deployed", _tld);
+    }
+
+    // This function will give us the price of a domain based on length
+    function price(string calldata name) public pure returns(uint) {
+        uint len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+        return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+        } else if (len == 4) {
+        return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+        } else {
+        return 1 * 10**17;
+        }
+    }
 
     struct Domain {
         address registrant;
@@ -15,23 +38,20 @@ contract Domains {
 
     // custom "mapping" data type to store their names
     mapping(string => Domain) public domains;
-
-    // A "mapping" data type to store their names
-	// mapping(string => address) public domains;
-
-	// Checkout our new mapping! This will store values
-    // mapping(string => string) public records;
-    
-    constructor() {
-        console.log("BREAKING THIS BETTER. HOLD FOR ERROR RESOLUTION.");
-    }
     
     // A register function that adds their names to our mapping
-    function register(string calldata name) public {
+    function register(string calldata name) public payable{
         // Check that the name is unregistered
         require(domains[name].registrant == address(0));
+
+        uint _price = price(name);
+
+        // Check if enough Matic was paid in the transaction
+        require(msg.value >= _price, "Not enough Matic paid");
+
         domains[name].registrant = msg.sender;
         domains[name].controller = msg.sender;
+        
         console.log("%s has registered a domain!", msg.sender);
     }
 
--- cool-domains/scripts/run.js
+++ cool-domains/scripts/run.js
@@ -2,48 +2,55 @@ const main = async () => {
 	// The first return is the deployer, the second is a random account
 	const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-	const domainContract = await domainContractFactory.deploy();
+  // We pass in "ly" to the constructor when deploying
+	const domainContract = await domainContractFactory.deploy("ly");
 	await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
 	console.log("Contract deployed by:", owner.address);
 	
-	let txn = await domainContract.register("doom");
+  console.log("\n...Registering and setter/getter functions...")
+
+  // We're passing in a second variable - value. This is the moneyyyyyyyyyy
+	let txn = await domainContract.register("surprising", {value: hre.ethers.utils.parseEther('0.1')});
 	await txn.wait();
 
-  const domainAddress = await domainContract.getAddress("doom");
-  console.log("Owner of domain doom:", domainAddress);
+  const domainAddress = await domainContract.getAddress("surprising");
+  console.log("Owner of domain surprising:", domainAddress);
 
-  let txnSetEmail = await domainContract.setEmail("doom", "test@testemail.com");
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
+  let txnSetEmail = await domainContract.setEmail("surprising", "test@testemail.com");
   await txnSetEmail.wait();
 
-  const domainEmail = await domainContract.getEmail("doom");
-  console.log("Email of domain doom:", domainEmail);
+  const domainEmail = await domainContract.getEmail("surprising");
+  console.log("Email of domain surprising:", domainEmail);
 
-  let txnSetContent = await domainContract.setContent("doom", "doom is when everyone only eats spam");
+  let txnSetContent = await domainContract.setContent("surprising", "surprising.ly this works");
   await txnSetContent.wait();
 
-  const domainContent = await domainContract.getContent("doom");
-  console.log("Content of domain doom:", domainContent);
+  const domainContent = await domainContract.getContent("surprising");
+  console.log("Content of domain surprising:", domainContent);
 
 	// Trying to set a record that doesn't belong to me!
-  console.log("...Testing set functions from another user")
+  console.log("\n...Testing set functions from another user...")
 
   let testContent = "Haha my domain now!";
   try {
-    let txnOtherSetContent = await domainContract.connect(randomPerson).setContent("doom", testContent);
+    let txnOtherSetContent = await domainContract.connect(randomPerson).setContent("surprising", testContent);
   }
   catch(e) {
     console.log(e.message);
-    console.log("content: '", testContent, "' not set");
+    console.log("content: '%s' not set", testContent);
   }
 
   let testEmail = "ScammyMcScammer@scammers-inc.evil";
   try {
-    let txnOtherSetEmail = await domainContract.connect(randomPerson).setEmail("doom", testEmail);
+    let txnOtherSetEmail = await domainContract.connect(randomPerson).setEmail("surprising", testEmail);
   }
   catch(e) {
     console.log(e.message);
-    console.log("Email: '", testEmail, "' not set");
+    console.log("Email: '%s' not set", testEmail);
   }
 
 }
