9b6e22243018fb16ad56979779c0f4e46ea9f0d6
Charles Dellinger <charles.dellinger@gmail.com>
2022-02-24 22:38:14 -0500

    copied in the boiler plate code for the react fronend and modified the name and description

---
+++ cool-domains/frontend/src/App.js
@@ -0,0 +1,65 @@
+import React,{ useEffect } from 'react';
+import './styles/App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+
+// Constants
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+	// Gotta make sure this is async.
+	const checkIfWalletIsConnected = () => {
+		// First make sure we have access to window.ethereum
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log("Make sure you have MetaMask!");
+			return;
+		} else {
+			console.log("We have the ethereum object", ethereum);
+		}
+	}
+
+	// Create a function to render if wallet is not connected yet
+	const renderNotConnectedContainer = () => (
+		<div className="connect-wallet-container">
+			<img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja gif" />
+			<button className="cta-button connect-wallet-button">
+				Connect Wallet
+			</button>
+		</div>
+  	);
+
+	// This runs our function when the page loads.
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, [])
+
+  return (
+		<div className="App">
+			<div className="container">
+
+				<div className="header-container">
+					<header>
+            <div className="left">
+              <p className="title">Adverb Name Service</p>
+              <p className="subtitle">The most modified API on the blockchain!</p>
+            </div>
+					</header>
+				</div>
+
+        <div className="footer-container">
+					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+					<a
+						className="footer-text"
+						href={TWITTER_LINK}
+						target="_blank"
+						rel="noreferrer"
+					>{`built with @${TWITTER_HANDLE}`}</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
